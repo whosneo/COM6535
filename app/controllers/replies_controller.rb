c954fc25f7c0a6dfc87db148a5a05db56952c9aa
Alexis Ioannou <aioannou2@sheffield.ac.uk>
2019-04-08 14:37:54 +0100

    fix bug with saving reply that would use last reply

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,7 +3,7 @@ class RepliesController < ApplicationController
   def create
     @post = Post.find(params.require(:reply).permit(:comment, :post_id, :original_id)[:post_id])
     @reply = @post.replies.create! allowed_params
-    @post = @post.replies.order(:created_at)
+    @replies = @post.replies.order(:created_at)
     respond_to do |f|
       f.html { redirect_to post_url}
       f.js
@@ -13,6 +13,6 @@ class RepliesController < ApplicationController
   private
 
   def allowed_params
-    params.require(:reply).permit(:comment, :post_id, :original_id).merge(user_id: current_user.id)
+    params.require(:reply).permit(:comment, :original_id, :post_id).merge(user_id: current_user.id)
   end
 end
