74d02ab5b386c19897a6d30b1de04fae8dff3cfb
Alexis Ioannou <aioannou2@sheffield.ac.uk>
2019-04-16 20:00:59 +0100

    Revert "Errbit configiration"
    This reverts commit 0f5fbb0feaf46358e83af5452a6de903a66b68cd.

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,27 +1,8 @@
-# ## Application deployment configuration
-# set :server,      'DEMO_SERVER'
-# set :user,        'DEMO_USER'
-# set :deploy_to,   -> { "/srv/services/#{fetch(:user)}" }
-# set :log_level,   :debug
-#
-# ## Server configuration
-# server fetch(:server), user: fetch(:user), roles: %w{web app db}
-#
-# ## Additional tasks
-# namespace :deploy do
-#   task :seed do
-#     on primary :db do within current_path do with rails_env: fetch(:stage) do
-#       execute :rake, 'db:seed'
-#     end end end
-#   end
-# end
-
 ## Application deployment configuration
-set :server,      'epi-stu-gen-demo2.shef.ac.uk'
-set :user,        'demo.team12'
+set :server,      'DEMO_SERVER'
+set :user,        'DEMO_USER'
 set :deploy_to,   -> { "/srv/services/#{fetch(:user)}" }
 set :log_level,   :debug
-set :rails_env,   :demo
 
 ## Server configuration
 server fetch(:server), user: fetch(:user), roles: %w{web app db}
