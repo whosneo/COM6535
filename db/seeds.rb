ccf6558794c8f544f46114629c63bf810dc90935
Alexis Ioannou <aioannou2@sheffield.ac.uk>
2019-04-16 20:55:07 +0100

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user_list = [ ['Alexis', 'Ioannou', 'Cyprus', 'Limassol', 'ioannou.alexis95@gmail.com', 'acp18ai', 'qweqweqwe']]
+
+
+
+user_list.each do |firstname, lastname, country, city, email, username, password|
+  User.create( firstname: firstname, lastname: lastname, location: country, city: city, email: email, username: username, password: password)
+end
+
+user1 = User.first
+
+post_list = [ ['This is a post about fitness', 'Describing something about fitness with a lot of words, this should be a bit long', 0],
+              ['A post about exercises', 'How do you obtain abs?', 0],
+              ['This is a post about diet', 'You should eat healthy to be healthy and fit', 1]]
+
+post_list.each do |title, description, type|
+  Post.create( user: user1, title: title, description: description, post_type: type)
+end
+
+post1 = Post.first
+
+Like.create(user: user1, post: post1, like: true)
