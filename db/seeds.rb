201d2430bd0314efbc540d0f2f59c7b0a5e813c0
Bowen Yu <byu8@sheffield.ac.uk>
2019-04-23 00:12:26 +0100

    chore(seed): generate 900 posts in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,23 +8,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-user_list = [ ['Alexis', 'Ioannou', 'Cyprus', 'Limassol', 'ioannou.alexis95@gmail.com', 'acp18ai', 'qweqweqwe', true]]
-
-
+user_list = [['Alexis', 'Ioannou', 'Cyprus', 'Limassol', 'ioannou.alexis95@gmail.com', 'acp18ai', 'qweqweqwe', true]]
 
 user_list.each do |firstname, lastname, country, city, email, username, password, admin|
-  User.create( firstname: firstname, lastname: lastname, location: country, city: city, email: email, username: username, password: password, admin: admin, confirmed_at: Time.now)
+  User.create(firstname: firstname, lastname: lastname, location: country, city: city, email: email, username: username, password: password, admin: admin, confirmed_at: Time.now)
 end
 
 user1 = User.first
 
-post_list = [ ['This is a post about fitness', 'Describing something about fitness with a lot of words, this should be a bit long', 0],
-              ['A post about exercises', 'How do you obtain abs?', 0],
-              ['This is a post about diet', 'You should eat healthy to be healthy and fit', 1]]
+post_list = [['This is a post about fitness', 'Describing something about fitness with a lot of words, this should be a bit long', 0],
+             ['A post about exercises', 'How do you obtain abs?', 0],
+             ['This is a post about diet', 'You should eat healthy to be healthy and fit', 1]]
 
 post_list.each do |title, description, type|
-  Post.create( user: user1, title: title, description: description, post_type: type)
+  (1..3_00).each { |_i| Post.create(user: user1, title: title, description: description, post_type: type) }
 end
 
 post1 = Post.first
