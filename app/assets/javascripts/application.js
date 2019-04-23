4b70f898ff7f990f9372b87bf66199618544921d
Alexis Ioannou <aioannou2@sheffield.ac.uk>
2019-04-23 15:24:50 +0100

    changing let in js to var

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,33 +20,33 @@ function showLoginMessage() {
 function showBlockedMessage() {
     $.flashAlert('Your account has been blocked due to inappropriate behaviour', 'alert');
 }
-//
-// function toggleSortButtonTime() {
-//     let sortTime = $('#sort_time');
-//     sortTime.toggleClass('down');
-//     if (sortTime.hasClass('down')) {
-//         sortTime.html('Time ⬇️');
-//     } else {
-//         sortTime.html('Time ⬆️');
-//     }
-// }
-//
-// function toggleSortButtonComments() {
-//     let sortComments = $('#sort_comments');
-//     sortComments.toggleClass('down');
-//     if (sortComments.hasClass('down')) {
-//         sortComments.html('Comments ⬇️');
-//     } else {
-//         sortComments.html('Comments ⬆️');
-//     }
-// }
-//
-// function toggleSortButtonLikes() {
-//     let sortLikes = $('#sort_likes');
-//     sortLikes.toggleClass('down');
-//     if (sortLikes.hasClass('down')) {
-//         sortLikes.html('Likes ⬇️');
-//     } else {
-//         sortLikes.html('Likes ⬆️');
-//     }
-// }
+
+function toggleSortButtonTime() {
+    var sortTime = $('#sort_time');
+    sortTime.toggleClass('down');
+    if (sortTime.hasClass('down')) {
+        sortTime.html('Time ⬇️');
+    } else {
+        sortTime.html('Time ⬆️');
+    }
+}
+
+function toggleSortButtonComments() {
+    var sortComments = $('#sort_comments');
+    sortComments.toggleClass('down');
+    if (sortComments.hasClass('down')) {
+        sortComments.html('Comments ⬇️');
+    } else {
+        sortComments.html('Comments ⬆️');
+    }
+}
+
+function toggleSortButtonLikes() {
+    var sortLikes = $('#sort_likes');
+    sortLikes.toggleClass('down');
+    if (sortLikes.hasClass('down')) {
+        sortLikes.html('Likes ⬇️');
+    } else {
+        sortLikes.html('Likes ⬆️');
+    }
+}
