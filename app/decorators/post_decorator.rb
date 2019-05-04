3ae22be8bc9b42f07c158f22bb942025bed67aea
Bowen Yu <byu8@sheffield.ac.uk>
2019-05-04 16:49:21 +0100

    refactor: remove unnecessary code

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -37,8 +37,6 @@ class PostDecorator < Draper::Decorator
       h.link_to 'Reply', h.show_reply_modal_reply_path(model.id, is_post: 1), method: :post, remote: true, class: "btn #{btn_class(model.post_type)} btn-lg btn-block fa fa-comment"
     elsif h.user_signed_in? && h.current_user.blocked?
       h.link_to 'Reply', 'javascript: showBlockedMessage()', class: "btn #{btn_class(model.post_type)} btn-lg btn-block fa fa-comment"
-    else
-      h.link_to 'Reply', 'javascript: showLoginMessage()', class: 'fa fa-comment'
     end
   end
 
