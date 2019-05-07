2873e0208186590e0e0348b72180fb7f8bc93e6e
Bowen Yu <byu8@sheffield.ac.uk>
2019-05-07 23:48:51 +0100

    feat: show more posts on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
 
   def home
     # @most_voted_posts = Post.select("posts.*, COUNT(likes.id) as likes_count").joins("LEFT OUTER JOIN likes ON (likes.post_id = posts.id)").group("posts.id").order('likes_count DESC').limit(3)
-    @most_voted_posts = Post.includes(:user).left_outer_joins(:likes).select('posts.*, COUNT(likes.id) as likes_count').where(likes: { like: true }).group('posts.id').order('likes_count DESC').limit(3)
+    @most_voted_posts = Post.includes(:user).left_outer_joins(:likes).select('posts.*, COUNT(likes.id) as likes_count').where(likes: { like: true }).group('posts.id').order('likes_count DESC').limit(5)
     @most_voted_posts = PostDecorator.decorate_collection(@most_voted_posts)
     # @most_voted_posts = @most_voted_posts.sort do |a, b|
     #   b.likes.count_likes <=> a.likes.count_likes
