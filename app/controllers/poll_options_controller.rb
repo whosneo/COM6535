2bd20ac09d5f74c6fdb9a6a34700f8bdd4fda69c
Bowen Yu <byu8@sheffield.ac.uk>
2019-05-08 17:45:56 +0100

    refactor: remove unnecessary code

--- app/controllers/poll_options_controller.rb
+++ app/controllers/poll_options_controller.rb
@@ -2,24 +2,5 @@
 
 # Poll Options controller
 class PollOptionsController < ApplicationController
-  def create
-    unless user_signed_in?
-      respond_to do |f|
-        f.js { render js: 'showLoginMessage()' }
-      end
-      return
-    end
-
-    if params.has_key?('poll_option')
-      PollOption.create(poll_option_params(params['poll_option']))
-    else
-      params['poll_options'].each do |poll_option|
-        if poll_option['title'] != ''
-          PollOption.create(poll_option_params(poll_option))
-        end
-      end
-    end
-
-    PollOption.create(title: title, post_id: params[:post_id])
-  end
+  before_action :authenticate_user!
 end
